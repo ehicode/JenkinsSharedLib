7108ebd9e925ecd108c510fd7cac3e4bfd5de8c5
ehicode <149331225+ehicode@users.noreply.github.com>
2023-12-13 23:38:25 -0500

    Update sharedlibone.groovy

---
+++ vars/sharedlibone.groovy
@@ -0,0 +1,10 @@
+def call (String stageName) {
+    if (stageName == "Build") {
+        sh 'mvn package'
+    } else if (stageName == "Deploy") {
+        sh 'mvn deploy'
+    } else {
+        echo "Invalid stageName: ${stageName}"
+        // You can add additional error handling logic here
+    }
+}
